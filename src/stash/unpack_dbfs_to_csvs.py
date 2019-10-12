3a49edff9596f77692083fecb90e6468a675754e
Dan Nguyen <dansonguyen@gmail.com>
2019-10-11 20:29:57 -0500

    fixed horrendous dbf typecasting issue, but data integrity questionable

--- src/stash/unpack_dbfs_to_csvs.py
+++ src/stash/unpack_dbfs_to_csvs.py
@@ -1,40 +1,59 @@
 #!/usr/bin/env python3
 import csv
-from dbfread import DBF
+from dbfread import DBF, FieldParser
+from sys import stderr
 from pathlib import Path
-from zipfile import ZipFile
 from tempfile import NamedTemporaryFile
+from zipfile import ZipFile
 
 SRC_DIR = Path('data', 'stashed', 'zips')
 SRC_FILES = SRC_DIR.glob('**/*.zip')
 DEST_DIR = Path('data', 'stashed', 'csvs')
+global _ERRS
+_ERRS = []
+
+"""these two files have errors
+data/stashed/csvs/2016/california.csv:478174
+data/stashed/csvs/2016/virginia.csv:19306
+"""
+
+
+class HpmsParser(FieldParser):
+    # because on 2016/california.dbf, we got this error:
+    # ~/.pyenv/versions/anaconda3-5.3.1/lib/python3.7/site-packages/dbfread/field_parser.py in parseN(self, field, data)
+    #     167         try:
+    # --> 168             return int(data)
+    #     169         except ValueError:
+
+    # ValueError: invalid literal for int() with base 10: b'_LA_SHERMA'
+    def parseN(self, field, value):
+        try:
+            x = FieldParser.parseN(self, field, value)
+        except ValueError as err:
+            x = value.decode('utf8')
+            global _ERRS
+            _ERRS.append([field.name, x])
+        finally:
+            return x
+    def parseF(self, field, value):
+        try:
+            x = FieldParser.parseF(self, field, value)
+        except ValueError as err:
+            x = value.decode('utf8')
+        finally:
+            return x
 
 
 def extract_dbf_bytes(zipname):
     # returns a temp filename
     z = ZipFile(zipname)
-    dbfpath = next(f.filename for f in z.filelist if '.dbf' in f.filename)
-    return z.read(dbfpath)
-    # f = parse_zipname(zipname)
-    # destdir = DEST_DIR.joinpath(f['year'])
-    # destdir.mkdir(exist_ok=True, parents=True)
-
-    # z = ZipFile(zipname)
-    # for x in z.filelist:
-    #     srcpath = x.filename
-    #     fileext = srcpath.split('.')[-1]
-    #     destname = f"{f['name']}.{fileext}"
-
-    #     destpath = destdir.joinpath(destname)
-    #     print("Extracting:", srcpath, "to:", destpath)
-    #     with open(destpath, 'wb') as w:
-    #         w.write(z.read(srcpath))
-
+    dbfpath = next((f.filename for f in z.filelist if '.dbf' in f.filename), None)
+    return z.read(dbfpath) if dbfpath else None
 
 def extract_records(dbfname):
-    d = DBF(dbfname, lowernames=True, load=False)
+    d = DBF(dbfname, lowernames=True, load=False, parserclass=HpmsParser)
     headers = [f.name for f in d.fields]
-    return (d.records, headers)
+    return (iter(d.records), headers, len(d.records))
 
 def parse_zipname(zipname):
     d = {}
@@ -43,20 +62,50 @@ def parse_zipname(zipname):
     return d
 
 
+def write_records_to_csv(destpath, records, fieldnames, rowcount):
+    global _ERRS
+    _ERRS = []
+    errors = []
+    destpath.parent.mkdir(exist_ok=True, parents=True)
+
+    with open(destpath, 'w') as w:
+        c = csv.DictWriter(w, fieldnames=fieldnames)
+        c.writeheader()
+        for i, r in enumerate(range(rowcount)):
+            c.writerow(next(records))
+
+            if _ERRS:
+                for e in _ERRS:
+                   msg = '|'.join([str(destpath), str(i)] + e)
+                   errors.append(msg)
+                _ERRS = []
+
+    tn.close()
+    return errors
+
 if __name__ == '__main__':
     for zn in SRC_FILES:
-        print("Opening:", zn)
-        tn = NamedTemporaryFile('wb')
-        tn.write(extract_dbf_bytes(zn))
-        records, fields = extract_records(tn.name)
-
         p = parse_zipname(zn)
         destpath = DEST_DIR.joinpath(p['year'], p['name'] + '.csv')
-        destpath.parent.mkdir(exist_ok=True, parents=True)
-        with open(destpath, 'w') as w:
-            print("Writing to:", destpath)
-            c = csv.DictWriter(w, fieldnames=fields)
-            c.writeheader()
-            for r in records:
-                c.writerow(r)
-            tn.close()
+
+        if destpath.exists():
+            print("Skipping: {}; this already exists: {}".format(zn, destpath))
+            continue
+
+        print("Opening:", zn)
+        dbytes = extract_dbf_bytes(zn)
+        if not dbytes:
+            # some zip files, such as pa_nhs national files, do not have a DBF
+            continue
+
+        tn = NamedTemporaryFile('wb')
+        tn.write(dbytes)
+        records, fields, rowcount = extract_records(tn.name)
+
+        print("Writing", rowcount, "records to:", destpath)
+
+        errors = write_records_to_csv(destpath, records, fields, rowcount)
+        if errors:
+            print("\tErrors found:", len(errors))
+            for e in errors:
+                stderr.write(e+"\n")
